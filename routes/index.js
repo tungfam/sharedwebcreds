a8f81bfdacb0678197d2d4fef02c8616be74cabf
Tung Fam <tungfam8@gmail.com>
2017-05-05 19:00:47 +0300

    add one more line to the file

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+		
 router.get('/apple-app-site-association', function(req, res, next) {
-	res.sendFile(path.join(__dirname, '../public', 'apple-app-site-association')
-}
+	res.sendFile(path.dirname(__dirname) + '/public/apple-app-site-association');
+});
 
 module.exports = router;
